ff92b9826dabfd3a6ace8371d7e59214feeb9a77
Dmitriy Fialkovskiy <dmitriy.fialkovskiy@gmail.com>
2022-04-05 07:42:03 +0300

    change input to txt file, add exclusions as file, loop search results, open ans switch tab to get info

--- src/main/java/app_main/Main.java
+++ src/main/java/app_main/Main.java
@@ -2,25 +2,40 @@ package app_main;
 
 import parser.Parser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
-        System.out.print("Enter key phrase:");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
-        System.out.print("Please enter user name : ");
+    public static void main(String[] args) throws InterruptedException, IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.forName("CP1251"))); //Charset.forName("CP1251") StandardCharsets.UTF_8
+//        System.out.print("Enter key phrase:");
+//        String keyphrase = null;
+//        try {
+//            keyphrase = reader.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         String keyphrase = null;
-        try {
-            keyphrase = reader.readLine();
+        BufferedReader br = Files.newBufferedReader(Paths.get("src/main/resources/input.txt"), Charset.forName("CP1251"));
+        keyphrase = br.readLine();
+
+        System.out.println("You entered : " + keyphrase);
+
+        List<String> exclusions = null;
+        try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/exclude.txt"), Charset.forName("CP1251"))) {
+            exclusions = stream.collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("You entered : " + keyphrase);
+
         Parser parser = new Parser();
-        parser.readPage(keyphrase);
+        parser.readPageAndSearchBy(new String(keyphrase.getBytes(Charset.forName("CP1251")), StandardCharsets.UTF_8), exclusions);
     }
 }
--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,14 +1,20 @@
 package parser;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Parser {
 
-    public void readPage(String keyphrase) throws InterruptedException {
+//    public WebDriver driver = new ChromeDriver();
+
+    public void readPageAndSearchBy(String keyphrase, List<String> exclusions) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         String baseUrl = "https://www.olx.kz/";
 
@@ -22,10 +28,41 @@ public class Parser {
         WebElement searchLaunchButton = driver.findElement(By.xpath("//*[@id=\"searchmain\"]/div/fieldset/span"));
         searchLaunchButton.click();
 
-        System.out.println("button pushed");
+        System.out.println("search submitted");
+
+        loopSearchResults(driver, exclusions);
 
         Thread.sleep(4000);
         driver.close();
+    }
+
+    public void loopSearchResults(WebDriver driver, List<String> exclusions) {
+        WebElement pagesContainer = driver.findElement(By.xpath("//div[contains(@class, 'pager') and contains(@class, 'rel') and contains(@class, 'clr')]"));
+        List<WebElement> pages = pagesContainer.findElements(By.xpath("//span[contains(@class, 'item') and contains(@class, 'fleft')]"));
+
+        String mainHandle= driver.getWindowHandle();
+        System.out.println("main handle" + mainHandle);
+
+        WebElement offersTable = driver.findElement(By.xpath("//*[@id=\"offers_table\"]"));
+        List<WebElement> offersList = offersTable.findElements(By.className("marginright5"));
+        Random random = new Random();
+        offersList.forEach(i -> {
+            if (exclusions.stream().noneMatch((new String(i.getText().toLowerCase().getBytes(StandardCharsets.UTF_8), Charset.forName("CP1251"))::contains))) {
+//                System.out.println(new String(i.getText().toLowerCase().getBytes(StandardCharsets.UTF_8), Charset.forName("CP1251")));
+                i.sendKeys(Keys.chord(Keys.CONTROL, Keys.ENTER));
+                try {
+                    Thread.sleep(random.nextInt(7000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String handle = driver.getWindowHandle();
+                System.out.println("curr handle" + handle);
+                List<String> handles = new ArrayList<>(driver.getWindowHandles()); //LinkedHashSet
+//                handles.forEach(System.out::println);
+                driver.switchTo().window(handles.get(handles.size() - 1));
+
+            }
+        });
     }
 
 }
